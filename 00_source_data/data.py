e6fc8a2812deadbc2df230ead2136a1112794967
Lorna <lorna1661maria@gmail.com>
2022-11-23 16:02:54 -0500

    updating the data generator py

--- 00_source_data/data.py
+++ 00_source_data/data.py
@@ -9,10 +9,26 @@ fips.sample(6)
 fips.columns
 
 #Opioids Description Dataset
-opioids_raw = pd.read_csv("https://d2ty8gaf6rmowa.cloudfront.net/dea-pain-pill-database/bulk/arcos_all_washpost.tsv.gz", compression="gzip", chunksize=5000000,usecols = ["BUYER_STATE","BUYER_COUNTY", "TRANSCATION_DATE", "CALC_BASE_WT_IN_GM","MME_Conversion_Factor"]) 
+# url = "https://d2ty8gaf6rmowa.cloudfront.net/dea-pain-pill-database/bulk/arcos_all_washpost.tsv.gz"
+url = r"C:\Users\ericr\Downloads\cmder\IDS720LEEP\pds-2022-leep\arcos_all_washpost.tsv"
 
+opioids_raw = pd.read_csv(url, chunksize= 1000000, sep="\t", iterator=True, usecols = ["BUYER_STATE","BUYER_COUNTY", "TRANSACTION_DATE", "CALC_BASE_WT_IN_GM","MME_Conversion_Factor"])
 states = ["FL","WA","TX"]
+tmp = []
+for data in opioids_raw:
+    tmpdata = data[data["BUYER_STATE"].isin(states)]
+    tmp.append(tmpdata)
+opioids_data = pd.concat(tmp)
+opioids_data.to_csv("opioids_data.csv", encoding='utf-8', index=False)
 
+#The populations 
+
+popstates = pd.read_csv('https://raw.githubusercontent.com/wpinvestigative/arcos-api/master/data/pop_states_20062014.csv')
+popstates.head(25)
+
+
+pop_counties = pd.read_csv('https://raw.githubusercontent.com/wpinvestigative/arcos-api/master/data/pop_counties_20062014.csv')
+pop_counties.head()
 
 
 
